a295524c5061fac26ce1f182782333353bdc0352
pedrobellinetti <51765524+pedrobellinetti@users.noreply.github.com>
2025-04-04 09:41:43 -0300

    Função UserRegistrationForm
    Cadastro do usuário - Inserção de nome, senha mestre e email. 
    
    ** Faz verificação, não deixa cadastrar uma senha com menos de 6 dígitos

--- SuperID/MainActivity.kt
+++ SuperID/MainActivity.kt
@@ -30,16 +30,16 @@ import com.google.firebase.ktx.Firebase
 import java.util.UUID
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             SuperIDTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    UserRegistrationForm(onSuccess = {
+                        Log.d("Cadastro", "Cadastro concluído")
+                    })
                 }
             }
         }
