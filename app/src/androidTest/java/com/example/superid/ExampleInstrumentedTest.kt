2727da9ba8cedd5ad7805de8733d317278706237
Pamorimd <priscilaa.amorin@outlook.com>
2025-04-20 20:38:40 -0300

    Melhora na UI e refatora a tela de cadastro de senhas da listagrem de senhas

--- app/src/androidTest/java/com/example/superid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/superid/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.projeto_integrador3
+package com.example.superid
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
